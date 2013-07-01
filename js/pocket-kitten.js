a267146120f5edf53b59132facf3ec4b58a7e10c
Rumyra <rumyra@gmail.com>
2013-07-01 16:31:45 +0100

    adding vibration and light sensitivity

--- js/pocket-kitten.js
+++ js/pocket-kitten.js
@@ -3,33 +3,42 @@ if (navigator.onLine == false) {
   alert('Oh nos! It seems you\'re not online :(\nSorry but this app only works if you are online, please connect and try again.');
 }
 
+var lightValue;
+
+window.addEventListener('devicelight', function(event) {
+  lightValue = event.value;
+});
 
 //creates url and loads image~~~~~~~~~~~~~~~~~~~~~~
-function setImage() {
+function setImage(lightValue) {
+  var urlOrigin = 'http://placekitten.com/';
+  if (lightValue < 50) {
+    urlOrigin = 'http://placepuppy.it/';
+  } 
   //get value between 240 and 440 for the width
   var imageWidth = Math.floor(Math.random() * (200 + 1)) + 240;
   //get value between 320 and 520 for the height
   var imageHeight = Math.floor(Math.random() * (200 + 1)) + 320;
 
   //Create image source url
-  var sourceUrl = 'http://placekitten.com/'+imageWidth+'/'+imageHeight;
+  var sourceUrl = urlOrigin+imageWidth+'/'+imageHeight;
   //ge image element
   var domElement = document.getElementById("kitten-image"); 
 
   //set new image source
   domElement.src = sourceUrl;
+
+  navigator.vibrate(200);
 }
 
 //run once on app load
 setImage();
 //run when clicked/tapped
 document.body.addEventListener('click', function onKittenSmashed(evt) {
-  setImage();
-  navigator.vibrate(200);
+  setImage(lightValue);
 });
 document.body.addEventListener('touchstart', function onKittenSmashed(evt) {
-  setImage();
-  navigator.vibrate(200);
+  setImage(lightValue);
 });
 
 //do install button shizzle~~~~~~~~~~~~~~~~~~~~~~~~~~~
